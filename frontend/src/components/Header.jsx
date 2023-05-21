ed260d623247166e1ba1b8c154ebb5da1a0508fd
jenny-hub90 <jumbledjenny19@gmail.com>
2023-05-21 20:57:28 +0545

    made login signup form functional

--- frontend/src/components/Header.jsx
+++ frontend/src/components/Header.jsx
@@ -1,9 +1,11 @@
-import { Navbar, Nav, Container } from 'react-bootstrap';
+import { Navbar, Nav, Container , NavDropdown, Badge} from 'react-bootstrap';
 import { FaSignInAlt, FaSignOutAlt } from 'react-icons/fa';
+import { useSelector, useDispatch } from 'react-redux';
 import { LinkContainer } from 'react-router-bootstrap';
 import { Link } from 'react-router-dom';
 
 const Header = () => {
+  const {userInfo} = useSelector((state) => state.auth)
   return (
     <header>
       <Navbar bg='dark' variant='dark' expand='lg' collapseOnSelect>
@@ -14,7 +16,22 @@ const Header = () => {
         <Navbar.Toggle aria-controls='basic-navbar-nav' />
           <Navbar.Collapse id='basic-navbar-nav'>
             <Nav className='ms-auto'>
-             <LinkContainer to='/login'>
+              {userInfo ? (
+                <>
+                 <NavDropdown title={userInfo.name} id='username'>
+                  <LinkContainer to='/profile'>
+                    <NavDropdown.Item>
+                      Profile
+                    </NavDropdown.Item>
+                  </LinkContainer>
+                    <NavDropdown.Item>
+                       Logout
+                    </NavDropdown.Item>
+                 </NavDropdown>
+                </>
+              ): (
+                <>
+                <LinkContainer to='/login'>
                 <Nav.Link>
                     <FaSignInAlt /> Sign In
                 </Nav.Link>
@@ -24,6 +41,9 @@ const Header = () => {
                     <FaSignOutAlt /> Sign  Up
                 </Nav.Link>
              </LinkContainer>
+                </>
+              )}
+           
             </Nav>
           </Navbar.Collapse>
         </Container>
